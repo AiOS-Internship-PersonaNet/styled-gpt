20d8265c2daa5f05c22610cdbff3e4995643fc55
Rob s <rsato@mail.sfsu.edu>
2023-12-04 22:48:58 -0800

    added chat model with stream and prompt

---
+++ chat_model.py
@@ -0,0 +1,37 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import (
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain.chat_models import ChatAnthropic
+from apikey import apikey
+
+llm = ChatOpenAI(openai_api_key=apikey)
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(
+            "You are a nice chatbot having a conversation with a human."
+        ),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{question}")
+    ]
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(
+    llm=llm,
+    prompt=prompt,
+    verbose=True,
+    memory=memory
+)
+# history = conversation({"question": "hi"})
+for chunk in conversation.stream({"question": "hi"}):
+    print(chunk, end="", flush=True)
+print("\n")
+# for ele in history:
+#     print("key: {}".format(history["text"]))
